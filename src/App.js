2e20aa2d7e32221754081bc194fec641ca8fab08
Abhilash Reddy <abhilashmv@outlook.com>
2020-02-19 16:13:56 +0530

    Reorganizing code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
-import Menu from "./menu/menu";
+import Menu from "./components/menu/menu";
 
-import CompaniesIndex from './companies'
-import  NewCompany from './companies/new'
+import CompaniesIndex from './components/companies'
+import  NewCompany from './components/companies/new'
 
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 export default () => (
   <Router>
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Image, Menu } from "semantic-ui-react";
-import logo from '../assets/logo.png'
+import logo from '../../assets/logo.png'
 import {Link} from 'react-router-dom'
 export default () => (
   <Menu>
