f07af6ff623e224027c7da663d693fbda706c07b
Abhilash Reddy <abhilashmv@outlook.com>
2020-02-21 10:47:52 +0530

    Company list view changes

--- src/components/companies/companyList.js
+++ src/components/companies/companyList.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Segment, Pagination } from "semantic-ui-react";
+import { Segment, Pagination, Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as companyActions from "../../actions/companiesActions";
 import { Link } from "react-router-dom";
-import get from 'lodash/get'
+import get from "lodash/get";
 
 class CompanyList extends React.Component {
   componentDidMount() {
@@ -14,11 +14,35 @@ class CompanyList extends React.Component {
   renderCompany = company => {
     return (
       <Segment raised key={company.id}>
+        {/* <Link to={`/companies/edit/${company.id}`}>more</Link>
         <h1>
-          {company.company_name}{" "}
-          <Link to={`/companies/edit/${company.id}`}>more</Link>
+          {company.company_name} | {company.city}, {company.state}
         </h1>
-        {company.description}
+        {company.description} */}
+        <div className="ui grid">
+          <div className="twelve wide column">
+            <div className="ui grid">
+              <div className="eight wide column">
+                <h1>{company.company_name}</h1>
+              </div>
+              <div className="eight wide column">
+                <h2>
+                  {company.city}, {company.state}
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="four wide column">
+            <h4>
+              <Link to={`/companies/edit/${company.id}`}>more...</Link>
+            </h4>
+          </div>
+          <div className="ui grid">
+            <div className="sixteen wide column">
+              <h5>{company.description}</h5>
+            </div>
+          </div>
+        </div>
       </Segment>
     );
   };
@@ -33,13 +57,13 @@ class CompanyList extends React.Component {
 
   onPageChange = (e, page) => {
     this.props.fetchCompanies(page.activePage);
-  }
+  };
 
   renderPagination = () => {
     if (this.props.companiesData) {
       let { pagination } = this.props.companiesData;
-      let pageCount = get(pagination, 'pages', 0)
-      let currentPage = get(pagination, 'page', 0)
+      let pageCount = get(pagination, "pages", 0);
+      let currentPage = get(pagination, "page", 0);
       return (
         <Pagination
           activePage={currentPage}
